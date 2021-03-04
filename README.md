e0058273ccee2c667da63f822900c14b53612155
Amh <abaams78@gmail.com>
2021-03-04 16:37:07 -0600

    project scafold

---
+++ README.md
@@ -0,0 +1,1 @@
+# reactive-mong-kafka
---
+++ src/main/java/com/example/ReactiveMongoKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoKafkaApplication.class, args);
+	}
+
+}
